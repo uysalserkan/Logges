0f9468ba50a9f17a9f685397439adbb3e7405757
uysalserkan <serkan.uysal@sahibinden.com>
2022-07-11 23:52:46 +0300

    #5 Add PDF Extractor module.

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,12 @@ import datetime
 import matplotlib.pyplot as plt
 from rich.console import Console
 from rich.table import Table
+from reportlab.lib.enums import TA_LEFT, TA_CENTER
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.pagesizes import LETTER, inch
+from reportlab.platypus import (SimpleDocTemplate, Paragraph, PageBreak, Image, Spacer, TableStyle)
+from reportlab.platypus import Table as reportlabTable
+from reportlab.lib.colors import Color
 
 
 def get_saving_path(log_dir: bool = False):
@@ -51,7 +57,14 @@ def get_current_time_HM():
 
 
 def console_data(script_name: str) -> None:
-    """We are printing our logs on console with beauty of rich."""
+    """We are printing our logs on console with beauty of rich.
+
+    Params:
+        script_name (str): That contains the script name which is running on console.
+
+    Return:
+        None
+    """
     dir_path = get_saving_path()
 
     filename = f"{dir_path}/{script_name}_{datetime.datetime.today().strftime('%Y-%m-%d')}.log"
@@ -98,3 +111,91 @@ def console_data(script_name: str) -> None:
     total_length = type_counter[0] + type_counter[1] + type_counter[2]
     rich_console.print("[blue]████████████████[yellow]████████████████[red]██████████████████")
     rich_console.print(f"Info: %{round(type_counter[0]/total_length*100, 2)}\tWarning: %{round(type_counter[1]/total_length*100, 2)}\tError: %{round(type_counter[2]/total_length*100, 2)}")
+
+
+def to_pdf(script_name: str) -> None:
+    """Export the logs to a file with `.pdf` format.."""
+
+    def copyright_text() -> Paragraph:
+        """We are add a text on the page."""
+        uysaltext = 'All right reserved 2022 &copy;&nbsp;<a href="https://github.com/uysalserkan/Logges">Logges</a> - <strong><a href="https://github.com/uysalserkan">uysalserkan</a></strong>'
+        copyright_style = ParagraphStyle("copyright_style", fontSize=8, alignment=TA_CENTER)
+        uysaltext_p = Paragraph(uysaltext, copyright_style)
+        return uysaltext_p
+
+    type_colors = {
+        "INFO": "blue",
+        "WARNING": "orange",
+        "ERROR": "red"
+    }
+    dir_path = get_saving_path()
+
+    filename = f"{dir_path}/{script_name}_{datetime.datetime.today().strftime('%Y-%m-%d')}.log"
+
+    # Burada eklemeler yapılıyor..
+    page_elements = []
+
+    # Header Başlığı Ekleniyor.
+    header_text = f"{script_name}.py {datetime.datetime.today().strftime('%Y-%m-%d')} Logs"
+    header_style = ParagraphStyle("H1", fontSize=18, alignment=TA_LEFT)
+    header = Paragraph(header_text, header_style)
+    page_elements.append(header)
+    page_elements.append(Spacer(10, 20))
+
+    img = Image(f'{dir_path}/pie_chart.png')
+    img.drawHeight = 3.5 * inch
+    img.drawWidth = 5.5 * inch
+    page_elements.append(img)
+
+    table_data = []
+
+    column_styled_list = []
+    centered = ParagraphStyle(name="centered", alignment=TA_CENTER)
+    columns = ["Status", "Time", "Message"]
+
+    for each in columns:
+        column_text = f"<font size='12'><b>{each}</b></font>"
+        styled_column = Paragraph(column_text, centered)
+        column_styled_list.append(styled_column)
+    table_data.append(column_styled_list)
+
+    alignStyle = ParagraphStyle(name="data", alignment=TA_CENTER)
+
+    # Reading data başlıyor..
+    with open(f"{filename}", 'r') as file:
+        logs = file.readlines()
+        file.close()
+
+    # Reading data bitiyor..
+    for each_log in logs[::-1]:
+        log_type = each_log.split("\t")[0].split(" ")[0].replace('[', '').replace(']', '')
+        log_time = each_log.split("\t")[0].split(" ")[1].replace('[', '').replace(']', '')[0:-1]
+        log_msg = each_log.split("\t")[1]
+        log_msg.replace("\n", '')
+
+        color = type_colors[log_type]
+
+        each_row = []
+        for index, each_item in enumerate([log_type, log_time, log_msg]):
+            if index == 2:
+                table_text = f"<font color='{color}'>{each_item}</font>"
+            else:
+                table_text = f"<b>{each_item}</b>"
+            ptext = Paragraph(table_text, alignStyle)
+            each_row.append(ptext)
+        table_data.append(each_row)
+
+    table = reportlabTable(table_data, colWidths=[70, 70, 350])
+    tStyle = TableStyle([
+        ('ALIGN', (0, -1), (-1, -1), 'LEFT'),
+        ("ALIGN", (1, 0), (1, -1), 'RIGHT'),
+        ('LINEABOVE', (0, 0), (-1, -1), 1, Color(0.2, 0.3, 0.4)),
+        ('BACKGROUND', (0, 0), (-1, 0), Color(102 / 255, 191 / 255, 191 / 255)),
+    ])
+    table.setStyle(tStyle)
+    page_elements.append(table)
+
+    page_elements.append(copyright_text())
+    page_elements.append(PageBreak())
+    pdf_doc = SimpleDocTemplate(f"{dir_path}/{script_name}_{datetime.datetime.today().strftime('%Y-%m-%d')}.pdf", pagesize=LETTER)
+    pdf_doc.multiBuild(page_elements)
