a9c7badb644232f7520173e4a30e48cbcfdbf103
uysalserkan <serkan.uysal@sahibinden.com>
2022-07-13 17:32:15 +0300

    Update setup files.

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 """Setup tool for making a library."""
 from setuptools import setup, find_packages
 
+with open('requirements.txt', 'r') as file:
+    reqs = file.read().splitlines()
+    file.close()
+
+with open('README.md', 'r') as file:
+    long_desc = file.readlines()
+    file.close()
+
 setup(
     name="Logges",
     version="1.0.0",
@@ -11,6 +19,7 @@ setup(
     package_dir={'': 'src'},
     url="https://github.com/uysalserkan/Logges",
     description="A simple Logging tool can extract as Markdown, PDF, or print console.",
-    keywords="Log, Logges, uysal, Logger"
-    
+    long_description=long_desc,
+    keywords="Log, Logges, uysal, Logger",
+    install_requires=reqs,
 )
