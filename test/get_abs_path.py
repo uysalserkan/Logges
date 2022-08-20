8ed69fad4308efb0e78b87707c9be3035fc03234
Restyled.io <commits@restyled.io>
2022-08-20 19:16:10 +0000

    Restyled by autopep8

--- test/get_abs_path.py
+++ test/get_abs_path.py
@@ -1,10 +1,12 @@
 import inspect
 import os
 
+
 def abs_path():
     abs_path = os.path.abspath((inspect.stack()[0])[1])
     directory_of_1py = os.path.dirname(abs_path)
     return directory_of_1py
+
 
 def abs_path_2(filename=__file__):
     return os.path.abspath(filename)
