76838cd63df72b27bb8f6f2214178bd6c6c012c9
Restyled.io <commits@restyled.io>
2022-08-20 19:19:03 +0000

    Restyled by isort

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 """Setup tool for making a library."""
 from pathlib import Path
 
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
 import src.Logges as Logges
 
