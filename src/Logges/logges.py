ab714ed7d222d37e97900c8fa66dbbcadac86d13
Restyled.io <commits@restyled.io>
2022-08-28 13:24:45 +0000

    Restyled by yapf

--- src/Logges/logges.py
+++ src/Logges/logges.py
@@ -106,9 +106,9 @@ class Logges:
         log_file.writelines(msg + "\n")
 
     @staticmethod
-    def log(
-        msg: str, status: LogStatus = LogStatus.DEBUG, print_log: bool = True
-    ) -> None:
+    def log(msg: str,
+            status: LogStatus = LogStatus.DEBUG,
+            print_log: bool = True) -> None:
         r"""Log a string with status message, please do not use `\n` character in your strigs.
 
         Parameters:
@@ -135,8 +135,8 @@ class Logges:
         icons = Logges.LogStatus.get_icon_dict()
         type_counter = [0, 0, 0, 0, 0]
         md_file = os.path.join(
-            SAVINGPATH, get_daily_log_file_name(filename=FILENAME, markdown=True)
-        )
+            SAVINGPATH,
+            get_daily_log_file_name(filename=FILENAME, markdown=True))
         markdown_file = open(md_file, "w")
 
         filename = get_daily_log_file_name(filename=FILENAME)
@@ -145,7 +145,8 @@ class Logges:
         with open(log_path, "r") as file:
             logs = file.readlines()
             file.close()
-        only_filename = "_".join(filename.split("_")[1:]) + ".py".replace(".log", "")
+        only_filename = "_".join(filename.split("_")[1:]) + ".py".replace(
+            ".log", "")
         file_date = filename.split("_")[0]
 
         if get_current_platform_name() == "Windows":
@@ -154,11 +155,11 @@ class Logges:
             f"# {only_filename} {file_date} Logs :see_no_evil: :hear_no_evil: :speak_no_evil:\n"
         )
         markdown_file.writelines("![](pie_chart.png)\n")
-        markdown_file.writelines("|TYPE|TIME|MESSAGE|\n| :--: | :--: | :--: |\n")
+        markdown_file.writelines(
+            "|TYPE|TIME|MESSAGE|\n| :--: | :--: | :--: |\n")
         for each_log in logs[::-1]:
-            log_type = (
-                each_log.split("\t")[0].split(" ")[0].replace("[", "").replace("]", "")
-            )
+            log_type = (each_log.split("\t")[0].split(" ")[0].replace(
+                "[", "").replace("]", ""))
             if log_type == list(icons.keys())[0]:
                 type_counter[0] += 1
             elif log_type == list(icons.keys())[1]:
@@ -166,18 +167,13 @@ class Logges:
             elif log_type == list(icons.keys())[2]:
                 type_counter[2] += 1
 
-            log_time = (
-                each_log.split("\t")[0]
-                .split(" ")[1]
-                .replace("[", "")
-                .replace("]", "")[0:-1]
-            )
+            log_time = (each_log.split("\t")[0].split(" ")[1].replace(
+                "[", "").replace("]", "")[0:-1])
             log_msg = each_log.split("\t")[1]
             log_msg.replace("\n", "")
             try:
                 markdown_file.writelines(
-                    f"|{icons[log_type]} | {log_time} | {log_msg.strip()}| "
-                )
+                    f"|{icons[log_type]} | {log_time} | {log_msg.strip()}| ")
                 markdown_file.writelines("\n")
             except KeyError:
                 raise ("Please check your icon.")
