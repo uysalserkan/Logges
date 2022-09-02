ce5190535d8e50d52133b5ce3ec54f2716b953d4
uysalserkan <serkan.uysal@sahibinden.com>
2022-09-02 22:38:55 +0300

    Fix basic examples.

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -5,10 +5,10 @@ from other_module import other_method
 
 if __name__ == '__main__':
     Logges.setup()
-    Logges.log(log="A simple DEBUG method.", status=Logges.LogStatus.DEBUG)
-    Logges.log(log="A simple INFO method.", status=Logges.LogStatus.INFO)
-    Logges.log(log="A simple WARNING method.", status=Logges.LogStatus.WARNING)
-    Logges.log(log="A simple ERROR method.", status=Logges.LogStatus.ERROR)
-    Logges.log(log="A simple CRITICAL method.", status=Logges.LogStatus.CRITICAL)
+    Logges.log(msg="A simple DEBUG method.", status=Logges.LogStatus.DEBUG)
+    Logges.log(msg="A simple INFO method.", status=Logges.LogStatus.INFO)
+    Logges.log(msg="A simple WARNING method.", status=Logges.LogStatus.WARNING)
+    Logges.log(msg="A simple ERROR method.", status=Logges.LogStatus.ERROR)
+    Logges.log(msg="A simple CRITICAL method.", status=Logges.LogStatus.CRITICAL)
     other_method()
     Logges.export(markdown=True, pdf=True, log=True, zip=True)
