14de84e124d63bc5ad421c6cc6edbe7cab578962
uysalserkan <serkan.uysal@sahibinden.com>
2022-09-02 22:38:34 +0300

    Fix basic examples.

--- examples/other_module.py
+++ examples/other_module.py
@@ -4,4 +4,4 @@ from Logges import Logges
 
 def other_method():
     """Amazing eExample method."""
-    Logges.log(log="This log called from other module.", status=Logges.LogStatus.ERROR)
+    Logges.log(msg="This log called from other module.", status=Logges.LogStatus.ERROR)
